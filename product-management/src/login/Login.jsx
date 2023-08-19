bcc568455ea97136ad2b02ab0d98dfbf7310323d
nagesh <nageshyour@gmail.com>
2023-08-19 09:26:46 +0530

    added states

--- product-management/src/login/Login.jsx
+++ product-management/src/login/Login.jsx
@@ -1,53 +1,88 @@
 import React, { useState } from 'react';
 
 export function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
+  const [userDetails, setUserDetails] = useState({
+    username: '',
+    password: '',
+    age: '',
+    dob:''
+  });
+
+  const [enableForm, setEnableForm] = useState(true);
+
+  // const [username, setUsername] = useState('');
+  // const [password, setPassword] = useState('');
+
+  const handleChange = (event) => {
+    setUserDetails({ ...userDetails, [event.target.id]: event.target.value }); // {username:'',password:'',username:'n'} //-->> { username: 'n',password:'' }
   };
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+  // const handlePasswordChange = (event) => {
+  //   setUserDetails({...userDetails, password:event.target.value});
+  // };
 
   const handleSubmit = (event) => {
-    event.preventDefault();
+    // event.preventDefault();
 
-    console.log('Username:', username);
-    console.log('Password:', password);
+    // console.log('Username:', username);
+    // console.log('Password:', password);
 
-    // Reset form fields to empty after submission
-    setUsername('');
-    setPassword('');
+    // // Reset form fields to empty after submission
+    // setUsername('');
+    // setPassword('');
   };
 
+
+  const handleDisableButton = () => {
+    setEnableForm(!enableForm);
+  }
+
   return (
-    <div><center>      
-      <h2>Login</h2>
+    <div>
+      <center>      
+        <h2>Login</h2>
+        <div>
+          <button onClick={handleDisableButton}>{enableForm ? 'Disable' : 'Enable'}</button>
+          {/* condition?true:false */}
+        </div>
+
+        
       <form onSubmit={handleSubmit}>
         <div className='username'>
-          <label>Username:  </label>
-          <input
-            type="text"
-            value={username}
-            onChange={handleUsernameChange}
-          /><br></br>
-        </div>
-        <div className='password'>
-          <label>Password:   </label>
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-          /><br></br>
-        </div>
-        <div classname='btn btn-primary'><button type="submit">Login</button></div>
-        <div classname='cancel'><button type="submit">Cancel</button></div>
-      </form>
-      </center>
+          <label htmlFor='username'>Username:  </label>
+            <input
+            id='username'
+            type="text"  
+            disabled={!enableForm}  
+            value={userDetails.username}
+            onChange={handleChange}
+          />          
+          </div>
 
+          <br/>
+          
+        <div className='password'>
+          <label htmlFor='password'>Password:   </label>
+            <input
+            id='password'
+              type="password"
+            disabled={!enableForm}  
+              
+            value={userDetails.password}
+            onChange={handleChange}
+            />
+          </div>
+          
+          <br/>
+          <div className='btn-section'>            
+            <button disabled={!enableForm}   classname='btn cancel' type="submit">Cancel</button>
+            <button disabled={!enableForm}   className='btn' type="submit" >Login</button>           
+          </div>
+        </form>
+        
+        {userDetails.username}--{userDetails.password}
+      </center>
     </div>
   );
 }
