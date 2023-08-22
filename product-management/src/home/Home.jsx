8af354f901175c530988505e3b54699971286da6
nagesh <nageshyour@gmail.com>
2023-08-22 21:56:38 +0530

    added table code

--- product-management/src/home/Home.jsx
+++ product-management/src/home/Home.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import "./Home.css";
+
 const url = "https://fakestoreapi.com/products";
 
 export function Home() {
@@ -16,29 +18,55 @@ export function Home() {
         return response.json();
       })
       .then((data) => {
-        setProducts(data);
+        const uData = Array.isArray(data) ? data : [data];
+        setProducts(uData);
       });
   };
 
-  //Mounting phase
-//   useEffect(() => {
-//     getProducts(null);
-//   }, []);
+  // Mounting phase
+  useEffect(() => {
+    getProducts(null);
+  }, []);
 
   //Updating Phase
   useEffect(() => {
     // if (productId > 0) {
-      getProducts(productId);
+    getProducts(productId);
     // }
   }, [productId]);
 
   //Unmounting Phase
+  useEffect(() => {
+    return () => {
+      console.log("component unmounting..");
+    };
+  }, []);
 
   return (
     <div>
       <input type="number" onChange={handleProductIdChange} />
-
-      {JSON.stringify(products)}
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((x) => {
+            return (
+              <tr key={x.id}>
+                <td>{x.id}</td>
+                <td>{x.title}</td>
+                <td>{x.category}</td>
+                <td>{x.price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
--- product-management/src/navbar/Navbar.jsx
+++ product-management/src/navbar/Navbar.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './navbar.css';
-
-
+import "./navbar.css";
+//presentational/stateless/dummy components
 export const Navbar = () => {
-return <div className="navbar-conatiner">       
-  <div className="item"><Link to={'/'}>Home</Link></div>
-  <div className="item"><Link to={'/login'}>Login</Link></div>
-  <div className="item"><Link to={'/register'}>Register</Link></div>
-  <div className="item"><Link to={'/contact'}>Contact</Link></div>
-</div>
-}
+  return (
+    <div className="navbar-conatiner">
+      <div className="item">
+        <Link to={"/"}>Home</Link>
+      </div>
+      <div className="item">
+        <Link to={"/login"}>Login</Link>
+      </div>
+      <div className="item">
+        <Link to={"/register"}>Register</Link>
+      </div>
+      <div className="item">
+        <Link to={"/contact"}>Contact</Link>
+      </div>
+    </div>
+  );
+};
